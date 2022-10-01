3819323e8fd6e9efd3a8d9c4a0daf9db39d3857a
Stephen M. Cameron <stephenmcameron@gmail.com>
2022-10-01 11:31:19 -0400

    Allow sharps and flats
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- notedriller.c
+++ notedriller.c
@@ -16,12 +16,24 @@ int select_note(int last_note)
 	return new_note;
 }
 
+int select_sharp_flat(int note)
+{
+	char sfc[] = " #b";
+	int sf = rand() % 3;
+	if (sf == 1 && (note == 1 || note == 4)) /* can't sharp B or E */
+		sf = 0;
+	if (sf == 2 && (note == 2 || note == 5)) /* can't flat C or F */
+		sf = 0;
+	return sfc[sf];
+}
+
 int main(int argc, char *argv[])
 {
 	float bpm = 40.0;
         struct timeval tv;
 	useconds_t waittime_us;
 	int note;
+	char sharpflat;
 
         gettimeofday(&tv, NULL);
         srand(tv.tv_usec);
@@ -40,7 +52,8 @@ int main(int argc, char *argv[])
 	note = select_note(-1);
 	do {
 		note = select_note(note);
-		printf("%c", natural_notes[note]);
+		sharpflat = select_sharp_flat(note);
+		printf("%c%c", natural_notes[note], sharpflat);
 		fflush(stdout);
 		for (int i = 0; i < 12; i++) {
 			usleep(waittime_us);
